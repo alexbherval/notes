7dc6aafd7beda74601c6b81180d3196ea7aa2754
Alex Boges <87436065+alexbherval@users.noreply.github.com>
2023-06-10 12:53:02 -0300

    atualização
    as views estao confussas

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,8 @@
 //Importando o Mongo Cliente que esta no driver mongdb instalado na aplicação
 const {MongoClient} = require('mongodb');
 
-
 //Definindo URL de conexao e porta, após a porta é imprtante colocar o nome do banco.
 const url = "mongodb://127.0.0.1:27017/AbeuGed";
-
 let _db;
 
 const initDb = cb => {
@@ -16,14 +14,11 @@ const initDb = cb => {
     })
     .catch(err => {
       cb(err);
-    });
-
+    });    
 }
 
 const getDb = () => {
-
   return _db;
-
 }
 
 module.exports = {

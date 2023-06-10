bc722cebdc071d2d5a38d3c9d2d47db03dac94ad
Alex Boges <87436065+alexbherval@users.noreply.github.com>
2023-06-10 00:13:18 -0300

    Alteração na view

--- public/css/sidebars.js
+++ public/css/sidebars.js
@@ -11,3 +11,24 @@ console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
+
+function trocaTitle() {
+  var trocaNome = document.getElementById("PageTile");
+  var urlAtual = window.location.href;
+  var urlpartes = urlAtual.split("/");
+  var urlfinal = urlpartes.pop();
+  trocaNome.textContent = urlfinal
+  if(urlfinal == null || urlfinal <= 0){
+    urlfinal = "PAINEL"
+    trocaNome.textContent = urlfinal
+  }if (urlfinal === "notes"){
+    urlfinal = "Adicionando Comunicação Interna"
+    trocaNome.textContent = urlfinal
+
+  }else {
+    trocaNome.textContent = urlfinal
+  }
+  
+}
+
+
--- routes/notes.js
+++ routes/notes.js
@@ -20,15 +20,20 @@ router.post('/', function(req, res){
     const setorRemetente = data.setorRemetente;
     const setorDestinatario = data.setorDestinatario;
     const assunto = data.assunto;
-    // const dataSolicit = data.dataSolicit;
     const destinatario = data.destinatario;
+    if(!data.item > 0) {
+        const dataItens = [];
+        dataItens = data.item
+    }
+    // const dataSolicit = data.dataSolicit;
+    const dataItens = data.item;
 
     //Buscando o Banco de dados:
 
         db.getDb()
         .db()
         .collection('comunicInterno')
-        .insertOne({ description: {assunto: assunto, descritivo: description}, remetente: remetente,
+        .insertOne({ description: {assunto: assunto, descritivo: description, item: dataItens}, remetente: remetente,
                         setorRemetente: setorRemetente, destinatario: destinatario, setorDestinatario: setorDestinatario, 
                         dataSolicit: new Date, obsCi: obsCi  });
 
@@ -60,12 +65,23 @@ router.post('/update', function(req, res){
     const data = req.body;
     const id = new ObjectId(data.id);
     const title = data.title;
+    const obsCi = data.obsCi;
     const description = data.description;
+    const remetente = data.remetente;
+    const setorRemetente = data.setorRemetente;
+    const setorDestinatario = data.setorDestinatario;
+    const assunto = data.assunto;
+    const destinatario = data.destinatario;
+
+
+
     //Buscando o Banco de dados:
     db.getDb()
         .db()
         .collection('comunicInterno')
-        .updateOne({_id: id}, { $set: { title: title, description: description } });
+        .updateOne({_id: id}, { $set: { description: {assunto: assunto, descritivo: description, item: dataItens}, remetente: remetente,
+            setorRemetente: setorRemetente, destinatario: destinatario, setorDestinatario: setorDestinatario, 
+            dataSolicit: new Date, obsCi: obsCi  } });
 
     res.redirect(301, '/');
 });
